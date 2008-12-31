eff8d082601d5af76f0ca392e4631193b8ca7224
Joshua Rubin <joshua@rubixlinux.org>
2008-12-31 07:01:50 +0000

    Initial commit of yubikey module. The YubiKey module allows users to log into drupal sites using their YubiKey. The YubiKey is a cryptographically secure One Time Password (OTP) generator that looks like a small USB token.

---
+++ yubikey_admin.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  if(!$("#edit-yubikey-enable").is(':checked')) {
+    $('#api_fields_div').hide();
+  }
+  $('#edit-yubikey-enable').change(function() {
+    if($(this).is(':checked')) {
+      $('#api_fields_div').slideDown('fast');
+    } else {
+      $('#api_fields_div').slideUp('fast');
+    }
+  });
+});
