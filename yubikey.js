4e2323d32a81915a23e507354f86f0d2bb25e3d8
Todd E Johnson <todd@toddejohnson.net>
2011-02-09 03:26:58 +0000

    #1055686 Import from code.google.com/p/yubikey-drupal/ project

--- yubikey.js
+++ yubikey.js
@@ -1,54 +1,111 @@
-// $Id$
+function handleFocus() {
+  if($('#edit-name').length > 0) {
+    $("#edit-name")[0].focus();
+  } else if($('#edit-pass').length > 0 && $('#edit-yubikey-otp').length > 0) {
+    if($('#edit-pass')[0].offsetTop < $('#edit-yubikey-otp')[0].offsetTop) {
+      $('#edit-pass')[0].focus();
+    } else {
+      $('#edit-yubikey-otp')[0].focus();
+      $('#edit-yubikey-otp').keypress(function(event) {
+        var evt = event || window.event;
+        var code = evt.which || evt.keyCode || evt.charCode;
+        if (13 == code) {
+          if(evt.which){
+            evt.preventDefault();
+          } else if (evt.keyCode) {
+            evt.keyCode = 9;
+          } else {
+            evt.charCode = 9;
+          }
+          $('#edit-pass').focus();
+        }
+      });
+    }
+  } else if($('#edit-yubikey-otp').length > 0) {
+    $('#edit-yubikey-otp')[0].focus();
+  } else if($('#edit-pass').length > 0) {
+    $('#edit-pass')[0].focus();
+  }
+}
+
+$(document).ready(function() {
+  if($('#edit-yubikey-otp').length > 0) {
+    $('#edit-yubikey-otp')[0].value = "";
+  }
+  if($('#edit-yubikey-forgot-pass').length > 0) {
+    if($('#edit-yubikey-forgot-pass').is(':checked')) {
+      $('#yk_pass_div').hide();
+    }
+    $('#edit-yubikey-forgot-pass').click(function() {
+      if($(this).is(':checked')) {
+        $('#yk_pass_div').slideUp('fast');
+      } else {
+        $('#yk_pass_div').slideDown('fast');
+      }
+    });
+  }
+});
 
 Drupal.behaviors.yubikey = function (context) {
-  var $loginElements   = $("#edit-name-wrapper, #edit-pass-wrapper, li.openid-link, li.yubikey-link");
-  var $openidElements  = $("#edit-openid-identifier-wrapper, li.user-link");
-  var $yubikeyElements = $("#edit-yubikey-otp-wrapper, li.yubikey-user-link");
-  var $yubikeyLoginLink = $("li.yubikey-link");
+  var $yubikeyElements = $("#edit-yubikey-otp-wrapper");
+
+  if ($("#edit-openid-identifier.openid-processed").size()) {
+    // Hide YubiKey OTP block
+      $yubikeyElements.hide();
+  } else {
+    handleFocus();
+  }
+ 
+  $("li.openid-link:not(.yubikey-openid-processed)", context).addClass('yubikey-openid-processed').click(function() {
+      // Hide YubiKey OTP block
+      $yubikeyElements.hide();
+      // Remove possible error message.
+      $("#edit-yubikey-otp").removeClass("error");
+      return false;
+    });
+  $("li.user-link:not(.yubikey-openid-processed)", context).addClass('yubikey-openid-processed').click(function() {
+      // Show YubiKey OTP block
+      $yubikeyElements.show();
+      handleFocus();
+      return false;
+    });
+};
+
+Drupal.behaviors.openid = function (context) {
+  var $loginElements = $("#edit-name-wrapper, #edit-pass-wrapper, li.openid-link");
+  var $openidElements = $("#edit-openid-identifier-wrapper, li.user-link");
 
   // This behavior attaches by ID, so is only valid once on a page.
-  if(!$("#edit-yubikey-otp.yubikey-processed").size() && $("#edit-yubikey-otp.error").length) {
-    $("#edit-yubikey-otp").addClass('yubikey-processed');
+  if (!$("#edit-openid-identifier.openid-processed").size() && $("#edit-openid-identifier").val()) {
+    $("#edit-openid-identifier").addClass('openid-processed');
     $loginElements.hide();
     // Use .css("display", "block") instead of .show() to be Konqueror friendly.
-    $yubikeyElements.css("display", "block");
+    $openidElements.css("display", "block");
   }
-
-  $("li.openid-link:not(.yubikey-openid-processed)", context).addClass('yubikey-openid-processed').click(function() {
-    // click openid link
-    $yubikeyElements.hide();
-    $yubikeyLoginLink.hide();
-  });
-
-  $("li.user-link:not(.yubikey-openid-processed)", context).addClass('yubikey-openid-processed').click(function() {
-    // click cancel openid link
-    $yubikeyElements.hide();
-    $yubikeyLoginLink.show();
-  });
-
-  $("li.yubikey-link:not(.yubikey-processed)", context).addClass('yubikey-processed').click(function() {
-    // click yubikey link
-    $loginElements.hide();
-    $openidElements.hide();
-    $yubikeyElements.css("display", "block");
-    // Remove possible error message.
-    $("#edit-name, #edit-pass").removeClass("error");
-    $("div.messages.error").hide();
-    // Set focus on YubiKey Identifier field.
-    $("#edit-yubikey-otp")[0].focus();
-    return false;
-  });
-
-  $("li.yubikey-user-link:not(.yubikey-processed)", context).addClass('yubikey-processed').click(function() {
-    // click cancel yubikey link
-    $openidElements.hide();
-    $yubikeyElements.hide();
-    $loginElements.css("display", "block");
-    // Clear YubiKey Identifier field and remove possible error message.
-    $("#edit-yubikey-otp").val('').removeClass("error");
-    $("div.messages.error").css("display", "block");
-    // Set focus on username field.
-    $("#edit-name")[0].focus();
-    return false;
-  });
+  $("li.openid-link:not(.openid-processed)", context)
+    .addClass('openid-processed')
+    .click( function() {
+       $loginElements.hide();
+       $openidElements.css("display", "block");
+      // Remove possible error message.
+      $("#edit-name, #edit-pass").removeClass("error");
+      $("div.messages.error").hide();
+      // Set focus on OpenID Identifier field.
+      $("#edit-openid-identifier")[0].focus();
+      return false;
+    });
+  $("li.user-link:not(.openid-processed)", context)
+    .addClass('openid-processed')
+    .click(function() {
+       $openidElements.hide();
+       $loginElements.css("display", "block");
+      // Clear OpenID Identifier field and remove possible error message.
+      $("#edit-openid-identifier").val('').removeClass("error");
+      $("div.messages.error").css("display", "block");
+      /*
+      // Set focus on username field.
+      //$("#edit-name")[0].focus();
+      */
+      return false;
+    });
 };
